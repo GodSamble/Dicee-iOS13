dd0f1316e0a8f41a17add38d4515233b5f72694e
Youngmin Ko <osgneb8359@naver.com>
2022-06-20 05:46:13 +0900

    첫번째 커밋

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //IBOutlet allows me to reference a UI element.
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            //WHO   .  WHAT    =   VALUE
+        diceImageView1.image=UIImage(named:"DiceSix")
+        diceImageView1.alpha=0.5
+        diceImageView2.image=UIImage(named:"DiceTwo")
     }
-
+    //임의로 지정하는 것
+    var leftDiceNumber=1
+    var rightDiceNumber=5
+    
+    //어플 내에 ROL 버튼 누를 시 무작위로 변환하는 코드
+    
+    @IBAction func rollBottonPressed(_ sender: Any) {
+        print("Button got tapped.")
+        let diceArray = [UIImage(named:"DiceOne"),UIImage(named:"DiceTwo"),UIImage(named:"DiceThree"),UIImage(named:"DiceFour"),UIImage(named:"DiceFive"),UIImage(named:"DiceSix")]
+        
+        diceImageView1.image=UIImage(named:"DiceFour")
+        diceImageView2.image=UIImage(named:"DiceFour")
+        diceImageView1.image=diceArray[Int.random(in:0...5)]
+        diceImageView2.image=diceArray[Int.random(in:0...5)]
+        
+        leftDiceNumber+=1
+        rightDiceNumber-=1
+    }
+    
 
 }
 
